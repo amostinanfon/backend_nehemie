bc07998f06d4aef4800923ae6f12bc2d7576fbe2
amostinanfon <amostinanfon17@gmail.com>
2022-07-11 07:58:07 +0100

    port set up

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.use('/api/orders', orderRoute);
 app.use('/api/checkout', stripeRoute); 
 
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Backend server is running")
 })
