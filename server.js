e80789ac2ae6ec814ac0f6ba934ee6eb37c169e1
amostinanfon <amostinanfon17@gmail.com>
2022-07-11 18:08:40 +0100

    adding cpule of site to to throught cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 dotenv.config();
 
-//app.use(cors());
+app.use(cors({origin:['https://sunset7.netlify.app', 'https:adminsun7.netlify.app'], credentials:true}));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Headers", 
-    'Origin,X-Requested-With,Content-Type, Accept, Authorization');
+    'Origin,X-Requested-With,Content-Type, Accept Authorization');
 
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE, GET');
